6ea6ca805df0d747ac91909628050a7b1d2a84a4
cody mcnutt <CodyMcNutt@codys-MacBook-Air.local>
2016-01-23 14:28:50 -0700

    fixed splash screen and header bar

--- public/Javascript/main.js
+++ public/Javascript/main.js
@@ -2,3 +2,14 @@ angular.module('umbrellaApp', [])
 
 angular.module('umbrellaApp')
 	.controller('umbrellaController', ['$scope', '$http', function($scope, $http){
+
+$scope.splash = true;
+
+  // Toggles the button
+  $scope.splashToggle = function() {
+
+    $scope.splash = !$scope.splash;
+
+  };
+
+  	}])
